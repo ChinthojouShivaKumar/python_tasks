10df70232c5189c0ca0390d9c617691e10e6a4ac
CHINTHOJU SHIVAKUMAR <124147339+ChinthojouShivaKumar@users.noreply.github.com>
2023-05-05 15:05:12 +0530

    Create ifelse.py

---
+++ ifelse.py
@@ -0,0 +1,40 @@
+weekDays = int(input("enter number to find week days"))
+
+if weekDays == 0:
+    print("sunday")
+elif weekDays == 1:
+    print("monday")
+elif weekDays == 2:
+    print("tuesday")
+elif weekDays == 3:
+    print("monday")
+elif weekDays == 4:
+    print("wednesday")
+elif weekDays == 5:
+    print("thursday")
+elif weekDays == 6:
+    print("friday")
+elif weekDays == 7:
+    print("sunday")
+else:
+    print("enter the input 1 to 6")
+    
+    
+    output:-
+      
+      PS D:\anjela\python> & C:/Users/shiva/AppData/Local/Programs/Python/Python311/python.exe d:/anjela/python/ifelse.py
+enter number to find week days0
+enter the input 1 to 6
+PS D:\anjela\python> & C:/Users/shiva/AppData/Local/Programs/Python/Python311/python.exe d:/anjela/python/ifelse.py
+enter number to find week days0
+sunday
+PS D:\anjela\python> & C:/Users/shiva/AppData/Local/Programs/Python/Python311/python.exe d:/anjela/python/ifelse.py
+enter number to find week days8
+enter the input 1 to 6
+PS D:\anjela\python> & C:/Users/shiva/AppData/Local/Programs/Python/Python311/python.exe d:/anjela/python/ifelse.py
+enter number to find week days5
+thursday
+PS D:\anjela\python> & C:/Users/shiva/AppData/Local/Programs/Python/Python311/python.exe d:/anjela/python/ifelse.py
+enter number to find week days3
+monday
+PS D:\anjela\python> 
