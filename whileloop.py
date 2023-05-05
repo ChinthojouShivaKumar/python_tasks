c671797a12f72fbf369f6e36483a0f3c32ad6bd9
CHINTHOJU SHIVAKUMAR <124147339+ChinthojouShivaKumar@users.noreply.github.com>
2023-05-05 15:28:10 +0530

    Create whileloop.py

---
+++ whileloop.py
@@ -0,0 +1,24 @@
+num = int(input('enter a number'))
+
+
+while num<5 :
+    print('shiva')
+    num+=1
+
+        
+      
+   output:- 
+    PS D:\anjela\python> & C:/Users/shiva/AppData/Local/Programs/Python/Python311/python.exe d:/anjela/python/whileloop.py
+enter a number2
+shiva
+shiva
+shiva
+PS D:\anjela\python> & C:/Users/shiva/AppData/Local/Programs/Python/Python311/python.exe d:/anjela/python/whileloop.py
+enter a number5
+PS D:\anjela\python> & C:/Users/shiva/AppData/Local/Programs/Python/Python311/python.exe d:/anjela/python/whileloop.py
+enter a number0
+shiva
+shiva
+shiva
+shiva
+shiva
