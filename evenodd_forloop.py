7d787347795b3f37d0dc756ceb7a372bf093b9b8
CHINTHOJU SHIVAKUMAR <124147339+ChinthojouShivaKumar@users.noreply.github.com>
2023-05-05 15:47:23 +0530

    Create evenodd_forloop.py

---
+++ evenodd_forloop.py
@@ -0,0 +1,26 @@
+inPut = int(input('enter your input'))
+
+for i in range(inPut, 21):
+    if(i%2 == 0):
+        print(f'{i} even')
+    else:
+        print(f'{i} odd')
+        
+        
+        
+   output:-
+    
+    PS D:\anjela\python> & C:/Users/shiva/AppData/Local/Programs/Python/Python311/python.exe d:/anjela/python/evenodd.py
+enter your input4
+4 even
+5 odd
+6 even
+7 odd
+8 even
+9 odd
+10 even
+11 odd
+12 even
+13 odd
+14 even
+15 odd
