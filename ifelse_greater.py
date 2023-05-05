5ed2fe5f42b66581954700df980ef881501a2317
CHINTHOJU SHIVAKUMAR <124147339+ChinthojouShivaKumar@users.noreply.github.com>
2023-05-05 15:11:16 +0530

    Create ifelse_greater.py

---
+++ ifelse_greater.py
@@ -0,0 +1,18 @@
+number = int(input('enter your input number'))
+
+if number>10: 
+    print(f"the input {number} is greater than 10")
+else:
+    print(f"the input {number} is less than 10")
+    
+    
+    
+   output:-
+    
+    PS D:\anjela\python> & C:/Users/shiva/AppData/Local/Programs/Python/Python311/python.exe d:/anjela/python/ifelse_greater.py
+enter your input number11
+the input 11 is greater than 10
+PS D:\anjela\python> & C:/Users/shiva/AppData/Local/Programs/Python/Python311/python.exe d:/anjela/python/ifelse_greater.py
+enter your input number7
+the input 7 is less than 10
+PS D:\anjela\python> 
