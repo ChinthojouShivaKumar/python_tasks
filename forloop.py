ee773b4af7d1d272d9a974b504afcdb3dfcff9c1
CHINTHOJU SHIVAKUMAR <124147339+ChinthojouShivaKumar@users.noreply.github.com>
2023-05-05 15:15:57 +0530

    Create forloop.py

---
+++ forloop.py
@@ -0,0 +1,34 @@
+number = int(input('enter your input number'))
+num = int(input('enter the range'))
+for i in range(number, num):
+    print(i)
+    
+    
+    output:-
+      
+     PS D:\anjela\python> & C:/Users/shiva/AppData/Local/Programs/Python/Python311/python.exe d:/anjela/python/ifelse_greater.py
+enter your input number0
+enter the range8
+0
+1
+2
+3
+4
+5
+6
+7
+PS D:\anjela\python> & C:/Users/shiva/AppData/Local/Programs/Python/Python311/python.exe d:/anjela/python/ifelse_greater.py
+enter your input number5
+enter the range16
+5
+6
+7
+8
+9
+10
+11
+12
+13
+14
+15
+PS D:\anjela\python> 
